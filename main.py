4d5d19112113a24b26e225fa910da25fe3b768f6
flame_jun <flame_jun@naver.com>
2026-08-13 21:32:03 +0900

    Add text edit te1 and fix btn1 handler function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
-                             QMessageBox)
+                             QMessageBox, QPlainTextEdit)
 from PyQt5.QtGui import QIcon
 
 
@@ -11,11 +11,14 @@ class Calculator(QWidget):
         self.initUI()
         
     def initUI(self):
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
+        
         self.btn1=QPushButton('Message',self)
         self.btn1.clicked.connect(self.activateMessage)
         
         vbox=QVBoxLayout()
-        vbox.addStretch(1)
+        vbox.addWidget(self.te1)
         vbox.addWidget(self.btn1)
         vbox.addStretch(1)
         
@@ -27,7 +30,8 @@ class Calculator(QWidget):
         self.show()
         
     def activateMessage(self):
-        QMessageBox.information(self,"information","Button clicked!")
+        # QMessageBox.information(self, "information", "Button clicked!")
+        self.te1.appendPlainText("Button clicked!")
         
         
 if __name__ == '__main__':
